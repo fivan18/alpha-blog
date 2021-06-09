38fb85e6ac2aba05aa3a934fae30eaa0958560ad
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-06-09 00:28:18 -0500

    run rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,17 +18,17 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-        flash[:notice] = "Article was created successfully."
-        redirect_to @article
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
     else
-        render 'new'
+      render 'new'
     end
   end
 
   def update
     @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article was updated successfully."
+      flash[:notice] = 'Article was updated successfully.'
       redirect_to @article
     else
       render 'edit'
